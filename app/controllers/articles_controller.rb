985b23ca4d5791ab6c992ddc8588442dcb8127e2
Rick Wheeler <rickwheeler1992@gmail.com>
2020-07-04 12:33:01 -0700

    added first dry

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
-def show
-    @article = Article.find(params[:id])
-end
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+    def show
+   end
 
 def index
     @articles = Article.all 
@@ -12,11 +13,11 @@ def new
 end
 
 def edit
-    @article = Article.find(params[:id])
+    
 end
 
 def create
-@article = Article.new(params.require(:article).permit(:title, :description))
+@article = Article.new(article_params) 
 if @article.save
     flash[:notice] = "Article was saved successfully."
 redirect_to @article
@@ -27,13 +28,26 @@ end
 end
 
 def update 
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
         flash[:notice] = "Article was updated successfully."
         redirect_to @article
     else
         render 'edit'
  
     end
+end
+
+def destroy
+    @article.destroy
+    redirect_to articles_path
+end
+
+private
+def set_article
+@article = Article.find(params[:id])
+end
+
+def article_params
+    params.require(:article).permit(:title, :description)
 end
 end
